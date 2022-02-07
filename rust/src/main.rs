15064156dfe72b616ab38178ad33c6112ba0a44e
David Rohmer <rohmer.david@gmail.com>
2022-02-07 21:34:04 +0100

    refactor roam dump types into module

---
+++ rust/src/main.rs
@@ -0,0 +1,43 @@
+mod roam_dump;
+
+use roam_dump::{RoamBlock, RoamPage};
+
+use std::{env, fs};
+use std::error::Error;
+use std::process::{exit};
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        println!("error: provide path to roam dump");
+        exit(1)
+    }
+
+    let contents = fs::read_to_string(&args[1]).expect("could not find dump file");
+    let dump: Vec<RoamPage> = serde_json::from_str(contents.as_str()).expect("failed to parse the loaded file...");
+
+    fn print_blocks(children: &Vec<RoamBlock>, depth: u32) {
+        let padding = match depth {
+            0 => String::new(),
+            _ => format!("{1:0$}", (depth * 4) as usize, " ")
+        };
+
+        for block in children {
+            println!("{}- {}", padding, block.string);
+            if let Some(children_nested) = &block.children {
+                print_blocks(children_nested, depth + 1);
+            }
+        }
+    }
+
+    for page in dump {
+        println!("Page: {}", page.title);
+        if let Some(children) = page.children {
+            print_blocks(children.as_ref(), 0)
+        }
+    }
+
+    Ok(())
+}
