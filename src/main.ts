7bb885e18839de69b93075211ce75ce953902d0d
David Rohmer <rohmer.david@gmail.com>
2022-03-04 14:22:05 +0100

    expose wasm as window.rwasm

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import App from './App.svelte';
 
+import * as rwasm from '../whale_rust_wasm/pkg'
+
+window['rwasm'] = rwasm // for convenience / debuggability
+
 const app = new App({
 	target: document.body
 });
 
 export default app;
+
