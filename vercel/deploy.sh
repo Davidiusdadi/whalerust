64fea593b76df4ede4bd84e9ddb2116d81cbfd36
David Rohmer <rohmer.david@gmail.com>
2022-04-02 18:29:55 +0200

    pass deploy.sh args on to vercel invocation

--- vercel/deploy.sh
+++ vercel/deploy.sh
@@ -4,4 +4,4 @@ set -e # exit on error
 cd -- "$(dirname "$0")"
 rm -r ./public || echo "nothing to clean"
 cp -r ../public ./public  # assumes the whole project has been build successfully already
-vercel
+vercel "$@"
