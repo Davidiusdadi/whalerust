d988cbad04bc6dc229a045e61e0b28ea24d17a08
David Rohmer <rohmer.david@gmail.com>
2022-04-08 04:22:05 +0200

    Make roam-loader generate obsidian/foam block ids when block-refs exist

--- src/db/roam-loader.ts
+++ src/db/roam-loader.ts
@@ -6,12 +6,27 @@ import type { Source } from 'src/db/file_source'
 import { tabSize } from 'src/store/defaults'
 
 export default function loadRoamData(source: Source, roam_raw_data: string): File[] {
-    const map_child = (b: RoamBlock, level = 0): string => {
-        const own = `${' '.repeat(level * tabSize)}- ${b.string}\n`
-        return own + (b.children?.map((c) => map_child(c, level + 1)).join('\n') ?? '')
+    const block_refs = new Set<string>()
+
+    const ref_scan_block = (b: RoamBlock) => {
+        b.refs?.forEach(r => block_refs.add(r.uid))
+        b.children?.map(b => ref_scan_block(b))
     }
 
     const pages = rust.parse_dump(roam_raw_data)
+    pages.forEach(p => {
+        p.children?.map(b => ref_scan_block(b))
+    })
+
+    const map_child = (b: RoamBlock, level = 0): string => {
+        let ref_id = ''
+        if (block_refs.has(b.uid)) {
+            ref_id = ` ^${b.uid}`
+        }
+        const own = `${' '.repeat(level * tabSize)}- ${b.string}${ref_id}\n`
+        return own + (b.children?.map((c) => map_child(c, level + 1)).join('\n') ?? '')
+    }
+
     return pages.map(p => {
         return new File({
                 source,
